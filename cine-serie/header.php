c9d69177c39ad70d693626e7caca52aab74efda4
Maron léonard <46001359+fekalink@users.noreply.github.com>
2019-05-08 22:17:36 +0200

    Add files via upload

---
+++ cine-serie/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/slate/bootstrap.min.css">
+    <link rel="stylesheet" href="css/globalCss.css">
+    <title>index</title>
+  </head>
+  <body>
+    <?php include 'navBar.php' ?>
---
+++ cine-serie/index.php
@@ -0,0 +1,39 @@
+<?php include 'header.php'; ?>
+
+    <div class="col-6 mx-auto d-flex justify-content-around mt-5">
+
+      <div>
+        <h1>To Do List</h1>
+        <ul>
+          <li>Ajouter un avis sur un film ou une serie</li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+
+      <div>
+        <h1>List des imports</h1>
+        <ul>
+          <li>Bootswatch => Slate</li>
+          <li>KoolAjax</li>
+          <li>Api Film</li>
+        </ul>
+      </div>
+
+    </div>
+
+    <br><br>
+
+    <div class="col-6 mx-auto">
+
+      <h4 class="text-center">Description :</h4>
+      <p class="text-center">Ce site permet de crée des article de critique de film/serie dont on veux partager son avis</p>
+
+      <br><br><br><hr>
+
+      <h2 class="text-center">Ici ce trouvera une page de login</h2>
+
+    </div>
+
+
+<?php include 'footer.php'; ?>
